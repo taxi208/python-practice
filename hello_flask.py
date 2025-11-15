7eb23e15016f79da34f21c7c6fc35dd471005e18
taxi208 <issey.rickowens@gmail.com>
2025-11-15 15:17:02 +0900

    git commit -m "add: Flaskフォーム実装と今日の学びをREADMEに追記"

---
+++ hello_flask.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    name = None
+    if request.method == "POST":
+        name = request.form.get("username")
+        return render_template("index.html", name=name)
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
